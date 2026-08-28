8818238cefbfd59e74c4ac83737f91dd885ad5e2
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2026-08-28 16:52:21 +0200

    Extract watchdog into watchdog.go

--- internal/jmap/watch.go
+++ internal/jmap/watch.go
@@ -484,38 +484,6 @@ func (self *WatchMailboxes) readEvents(ctx context.Context, r io.Reader,
 	}
 }
 
-func (self *WatchMailboxes) startWatchdog(ctx context.Context,
-) (deferFunc func()) {
-	d := self.watchdogInterval()
-	l := self.logger()
-	l.Info("start ping watchdog", slog.Duration("timeout", d))
-
-	self.watchdogTicker = time.NewTicker(d)
-	wg := new(sync.WaitGroup)
-	wg.Go(func() { self.watchdog(ctx) })
-
-	return func() {
-		self.stopWatchdog()
-		l.Info("waiting watchdog goroutine to stop...")
-		wg.Wait()
-	}
-}
-
-func (self *WatchMailboxes) watchdogInterval() time.Duration {
-	return time.Duration(self.pingInterval)*time.Second + time.Minute
-}
-
-func (self *WatchMailboxes) watchdog(ctx context.Context) {
-	select {
-	case <-ctx.Done():
-		self.watchdogTicker.Stop()
-		self.logger().Info("ping watchdog stopped", slog.Any("reason", ctx.Err()))
-	case <-self.watchdogTicker.C:
-		self.stopWatchdog()
-		self.logger().Info("ping watchdog timed out, reconnect to event source")
-	}
-}
-
 func (self *WatchMailboxes) dispatchEvent(eventType string, b *bytes.Buffer,
 	stateChange *jmap.StateChange, yield func(*jmap.StateChange, error) bool,
 ) bool {
